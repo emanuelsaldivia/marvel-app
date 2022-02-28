05abb1ac43df62dc28a4115fd2e9104e793c7e6f
emanuelsaldivia <emanuelsaldivia@gmail.com>
2022-02-28 10:53:35 -0300

    Network service created

---
+++ app/src/main/java/com/esaldivia/marvelheroes/network/CharacterNetworkService.kt
@@ -0,0 +1,17 @@
+package com.esaldivia.marvelheroes.network
+
+import com.esaldivia.marvelheroes.data.model.MarvelDataWrapper
+import com.esaldivia.marvelheroes.data.model.character.CharacterNetworkDto
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface CharacterNetworkService {
+
+    @GET("/v1/public/characters")
+    suspend fun getCharactersFromApi(
+        @Query("ts") timeStamp: Int = 1,
+        @Query("nameStartsWith") name: String? = null,
+        @Query("offset") offset: Int = 0
+    ): Response<MarvelDataWrapper<CharacterNetworkDto>>
+}

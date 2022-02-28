ac403a4467533ff2fe6345e0b8b77c864f1cd740
emanuelsaldivia <emanuelsaldivia@gmail.com>
2022-02-28 11:06:08 -0300

    Character model class created

---
+++ app/src/main/java/com/esaldivia/marvelheroes/data/model/character/Character.kt
@@ -0,0 +1,7 @@
+package com.esaldivia.marvelheroes.data.model.character
+
+data class Character(
+    val id: Int? = 0,
+    val name: String? = null,
+    val description: String? = null,
+    val thumbnailUrl: String? = null)

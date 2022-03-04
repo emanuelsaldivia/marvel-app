94f9056222ee407d952e714f40b760080f44422f
emanuelsaldivia <emanuelsaldivia@gmail.com>
2022-03-04 20:23:20 -0300

    New character list design

--- app/src/main/java/com/esaldivia/marvelheroes/ui/character/CharacterAdapter.kt
+++ app/src/main/java/com/esaldivia/marvelheroes/ui/character/CharacterAdapter.kt
@@ -2,7 +2,9 @@ package com.esaldivia.marvelheroes.ui.character
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.esaldivia.marvelheroes.R
 import com.esaldivia.marvelheroes.data.model.character.Character
 import com.esaldivia.marvelheroes.databinding.CharacterItemLayoutBinding
@@ -29,13 +31,14 @@ class CharacterAdapter(private val characterList: List<Character>) :
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(character: Character) {
-            binding.characterName.text = character.name
-            if (!character.description.isNullOrEmpty()) {
-                binding.characterDescription.text = character.description
-            } else {
-                binding.characterDescription.text =
-                    binding.root.context.getString(R.string.description_unavailable)
-            }
+            binding.itemCharName.text = character.name
+
+            val uri = character.thumbnailUrl?.toUri()?.buildUpon()?.scheme("https")?.build() ?: ""
+
+            Glide.with(binding.root.context)
+                .load(uri)
+                .error(R.drawable.marvel_logo)
+                .into(binding.itemCharImg)
         }
     }
 }

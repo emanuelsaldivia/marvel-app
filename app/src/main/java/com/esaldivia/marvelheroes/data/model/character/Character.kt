b43279cb2b43aa4cb30b32914776d9ef0dbf5083
emanuelsaldivia <emanuelsaldivia@gmail.com>
2022-03-05 13:29:53 -0300

    Added parcelize

--- app/src/main/java/com/esaldivia/marvelheroes/data/model/character/Character.kt
+++ app/src/main/java/com/esaldivia/marvelheroes/data/model/character/Character.kt
@@ -1,7 +1,11 @@
 package com.esaldivia.marvelheroes.data.model.character
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Character(
     val id: Int? = 0,
     val name: String? = null,
     val description: String? = null,
-    val thumbnailUrl: String? = null)
+    val thumbnailUrl: String? = null) : Parcelable

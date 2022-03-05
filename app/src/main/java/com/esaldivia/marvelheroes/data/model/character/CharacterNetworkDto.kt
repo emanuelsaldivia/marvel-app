50080f4d555d9742f7d8afae3135009580c217b8
emanuelsaldivia <emanuelsaldivia@gmail.com>
2022-03-05 19:12:00 -0300

    Showing character detail

--- app/src/main/java/com/esaldivia/marvelheroes/data/model/character/CharacterNetworkDto.kt
+++ app/src/main/java/com/esaldivia/marvelheroes/data/model/character/CharacterNetworkDto.kt
@@ -1,6 +1,7 @@
 package com.esaldivia.marvelheroes.data.model.character
 
 import com.esaldivia.marvelheroes.data.model.*
+import com.esaldivia.marvelheroes.data.model.SummaryNetworkDto
 import com.google.gson.annotations.SerializedName
 import java.util.*
 
--- app/src/main/java/com/esaldivia/marvelheroes/ui/character/CharacterDetailFragment.kt
+++ app/src/main/java/com/esaldivia/marvelheroes/ui/character/CharacterDetailFragment.kt
@@ -4,14 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.esaldivia.marvelheroes.R
 import com.esaldivia.marvelheroes.common.Constants
+import com.esaldivia.marvelheroes.common.Resource
 import com.esaldivia.marvelheroes.data.model.character.Character
 import com.esaldivia.marvelheroes.databinding.FragmentCharacterDetailBinding
+import com.esaldivia.marvelheroes.openMarvelScope
+import toothpick.ktp.KTP
+import toothpick.ktp.delegate.inject
+import toothpick.smoothie.lifecycle.closeOnDestroy
+import toothpick.smoothie.viewmodel.installViewModelBinding
 
 class CharacterDetailFragment : Fragment() {
     private var _binding: FragmentCharacterDetailBinding? = null
     private val binding get() = _binding!!
+
+    private val charactersViewModel: CharactersViewModel by inject()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,10 +33,51 @@ class CharacterDetailFragment : Fragment() {
         return binding.root
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        KTP.openMarvelScope()
+            .openSubScope(CharacterDetailFragment::class)
+            .installViewModelBinding<CharactersViewModel>(this)
+            .closeOnDestroy(this)
+            .inject(this)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val character = arguments?.getParcelable<Character>(Constants.CHARACTER_KEY)
-        character?.id
+
+        charactersViewModel.characterLiveData.observe(viewLifecycleOwner) {
+            when (it) {
+                is Resource.Success -> {
+                    binding.detailsNameTv.text = it.value?.name
+                    val description = if (!it.value?.description.isNullOrEmpty()) {
+                        it.value?.description
+                    } else{
+                        context?.getText(R.string.description_not_available)
+                    }
+                    binding.detailsDescriptionValueTv.text = description
+
+                    val uri =
+                        it.value?.thumbnailUrl?.toUri()?.buildUpon()?.scheme("https")?.build() ?: ""
+
+                    context?.let { context ->
+                        Glide.with(context)
+                            .load(uri)
+                            .error(R.drawable.marvel_logo)
+                            .into(binding.detailsCharacterPoster)
+                    }
+                }
+                is Resource.Error -> {
+                    // TODO
+                }
+                is Resource.Loading -> {
+                    // TODO
+                }
+            }
+        }
+
+        character?.id?.let { charactersViewModel.getCharacter(it) }
     }
 
     override fun onDestroy() {
